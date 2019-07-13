9fc4e73b169baceaca81fe2a49f7e716cc9d8b49
Josancev <josemanuel777@hotmail.com>
2019-07-13 00:57:38 +0000

    excuse generator

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,24 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let w = who.length;
+  let x = what.length;
+  let y = when.length;
+
+  let a = Math.round(Math.random() * w);
+  let b = Math.round(Math.random() * x);
+  let c = Math.round(Math.random() * y);
+
+  let t = who[a] + " " + what[b] + " " + when[c];
+  document.getElementById("excuse").innerHTML = t;
 };
